26c63a3ff2d5b31569f3f71dfac84e27df6dd873
Muntasir Syed <muntasir.syed@kcl.ac.uk>
2014-11-08 22:13:48 +0000

    Player game request is now 20 seconds

--- src/ConfirmDialog.java
+++ src/ConfirmDialog.java
@@ -16,7 +16,7 @@ public class ConfirmDialog extends JFrame implements ActionListener {
     private Player player;
     private Request input;
     private Timer time;
-    private int timeRemaining = 10;
+    private int timeRemaining = 20;
 
     public ConfirmDialog(final Player player, final Request input) {
 
