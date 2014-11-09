02f7f55bae5d89211280ed4448ca7b1401533eec
Muntasir Syed <muntasir.syed@kcl.ac.uk>
2014-11-09 17:31:40 +0000

    Cleanup newConnection() Thread code into it's own class

--- src/Player.java
+++ src/Player.java
@@ -40,130 +40,7 @@ public class Player{
             out.writeObject(new Request("UserJoinedLobby", name));
             out.writeObject(new Request("RetrieveLobby", name));
 
-            Thread receivingThread = new Thread() {
-                Request input;
-
-                @Override
-                public void run() {
-                    try {
-                        while ((input = (Request) in.readObject()) != null) {
-                            
-                            if (input.getActionType().equals("UserJoinedLobby")) {
-                                if (!input.getObject().equals(name)) {
-                                    lobbyFrame.addItem((String) input.getObject());
-                                }
-                            } 
-                            
-                            else if (input.getActionType().equals("UserLeftLobby")) {
-                            	lobbyFrame.deleteItem((String)input.getObject()); 
-                            } 
-                           
-                            else if (input.getDestination().equals(name)) {
-
-                                String actionType = input.getActionType();
-                                switch (actionType) {
-                                    case "GameRequest":
-                                        if (!isBusy) {
-                                            isBusy = true;
-                                            confirmDialog = new ConfirmDialog(Player.this, input);
-                                            confirmDialog.setVisible(true);
-                                            confirmDialog.setAlwaysOnTop(true);
-                                        } else {
-                                            out.writeObject(new Request("PlayerBusy", name, input.getOrigin()));
-                                        }
-                                        break;
-                                    case "GameRequestAnswer":
-                                        isBusy = false;
-                                        lobbyFrame.enablePlayButton(true);
-                                        if (input.getObject().equals("Yes")) {
-                                        	opponentName = input.getOrigin();
-                                            gameFrame();
-                                            isTheirTurn = true;
-                                        } else if (input.getObject().equals("No")) {
-                                            JOptionPane.showMessageDialog(null,
-                                                    "Game request denied from "
-                                                            + input.getOrigin()
-                                                            + ".",
-                                                    "Game request denied",
-                                                    JOptionPane.ERROR_MESSAGE);
-                                        }
-                                        break;
-                                    case "RetrieveLobby":
-                                        ArrayList<String> playersList = (ArrayList<String>) input.getObject();
-                                        lobbyFrame.updateLobby(playersList);
-                                        break;
-                                    case "ReceiveMessage":
-                                        gameUI.appendMessage((String) input.getObject(), input.getOrigin());
-                                        break;
-                                    case "UserLeftGame":
-                                    	System.out.println("backtolobby");
-                                        // Quitting game
-                                        reshowLobby();
-                                        JOptionPane.showMessageDialog(null, "Your opponent quit! You win (by default)", 
-                                                "Opponent Quit", JOptionPane.INFORMATION_MESSAGE);
-                                        break;
-                                    case "UserWentBackToLobby":
-                                        //Returning to lobby
-                                    	System.out.println("backtolobby");
-                                        reshowLobby();
-                                        JOptionPane.showMessageDialog(null, "Your opponent went back to lobby",
-                                                "Opponent Quit", JOptionPane.INFORMATION_MESSAGE);
-                                        break;
-                                    case "MoveResult":
-                                        GameMove gm = (GameMove) input.getObject();
-                                        Point coordinates = gm.getMoveCoordinates();
-                                        String playerName = gm.getPlayerName();
-                                        String outcome = gm.getMoveResult();
-
-                                        if (name.equals(playerName)) {
-                                            //update enemy board
-                                            if (outcome.equals("hit") || outcome.startsWith("destroyed")) {
-                                                isTheirTurn = true;
-                                                gameUI.startTimer();
-                                            }
-                                            gameUI.updateEnemyBoard(outcome, coordinates);
-                                        } else {
-                                            //update own board
-                                            if (!outcome.equals("hit") && !outcome.startsWith("destroyed")) {
-                                                isTheirTurn = true;
-                                                gameUI.appendMessage("It's your turn to play.", "GAME");
-                                                gameUI.startTimer();
-                                            }
-                                            gameUI.updateOwnBoard(outcome, coordinates);
-                                        }
-                                        break;
-                                    case "MoveEnded":
-                                        isTheirTurn = true;
-                                        gameUI.appendMessage("Enemy ran out of time.", "GAME");
-                                        gameUI.startTimer();
-                                        break;
-                                    case "GameStart":
-                                        shipPlacement.startGame();
-                                        break;
-                                    case "PlayerBusy":
-                                        isBusy = false;
-                                        lobbyFrame.enablePlayButton(true);
-                                        JOptionPane.showMessageDialog(null, "Player is busy. Please try again later",
-                                                "Error", JOptionPane.ERROR_MESSAGE);
-                                        break;
-                                    default:
-                                        System.out.println(input);
-                                        break;
-                                }
-                            }
-                        }
-                    } catch (EOFException e) {
-                        // EOFException - if this input stream reaches the end
-                        // before reading eight bytes
-                    } catch (ClassCastException e) {
-                        e.printStackTrace();
-                        System.out.println("An error occurred.");
-                        System.exit(1);
-                    } catch (ClassNotFoundException | IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
+            ReceivingThread receivingThread = new ReceivingThread();
             receivingThread.start();
         } catch (EOFException e) {
             // EOFException - if this input stream reaches the end before
@@ -174,6 +51,131 @@ public class Player{
                     "Failed to connect to the server", "Server Error",
                     JOptionPane.WARNING_MESSAGE);
 
+        }
+    }
+    
+    class ReceivingThread extends Thread {
+        Request input;
+
+        @Override
+        public void run() {
+            try {
+                while ((input = (Request) in.readObject()) != null) {
+
+                    if (input.getActionType().equals("UserJoinedLobby")) {
+                        if (!input.getObject().equals(name)) {
+                            lobbyFrame.addItem((String) input.getObject());
+                        }
+                    }
+
+                    else if (input.getActionType().equals("UserLeftLobby")) {
+                        lobbyFrame.deleteItem((String)input.getObject());
+                    }
+
+                    else if (input.getDestination().equals(name)) {
+
+                        String actionType = input.getActionType();
+                        switch (actionType) {
+                            case "GameRequest":
+                                if (!isBusy) {
+                                    isBusy = true;
+                                    confirmDialog = new ConfirmDialog(Player.this, input);
+                                    confirmDialog.setVisible(true);
+                                    confirmDialog.setAlwaysOnTop(true);
+                                } else {
+                                    out.writeObject(new Request("PlayerBusy", name, input.getOrigin()));
+                                }
+                                break;
+                            case "GameRequestAnswer":
+                                isBusy = false;
+                                lobbyFrame.enablePlayButton(true);
+                                if (input.getObject().equals("Yes")) {
+                                    opponentName = input.getOrigin();
+                                    gameFrame();
+                                    isTheirTurn = true;
+                                } else if (input.getObject().equals("No")) {
+                                    JOptionPane.showMessageDialog(null,
+                                            "Game request denied from "
+                                                    + input.getOrigin()
+                                                    + ".",
+                                            "Game request denied",
+                                            JOptionPane.ERROR_MESSAGE);
+                                }
+                                break;
+                            case "RetrieveLobby":
+                                ArrayList<String> playersList = (ArrayList<String>) input.getObject();
+                                lobbyFrame.updateLobby(playersList);
+                                break;
+                            case "ReceiveMessage":
+                                gameUI.appendMessage((String) input.getObject(), input.getOrigin());
+                                break;
+                            case "UserLeftGame":
+                                System.out.println("backtolobby");
+                                // Quitting game
+                                reshowLobby();
+                                JOptionPane.showMessageDialog(null, "Your opponent quit! You win (by default)",
+                                        "Opponent Quit", JOptionPane.INFORMATION_MESSAGE);
+                                break;
+                            case "UserWentBackToLobby":
+                                //Returning to lobby
+                                System.out.println("backtolobby");
+                                reshowLobby();
+                                JOptionPane.showMessageDialog(null, "Your opponent went back to lobby",
+                                        "Opponent Quit", JOptionPane.INFORMATION_MESSAGE);
+                                break;
+                            case "MoveResult":
+                                GameMove gm = (GameMove) input.getObject();
+                                Point coordinates = gm.getMoveCoordinates();
+                                String playerName = gm.getPlayerName();
+                                String outcome = gm.getMoveResult();
+
+                                if (name.equals(playerName)) {
+                                    //update enemy board
+                                    if (outcome.equals("hit") || outcome.startsWith("destroyed")) {
+                                        isTheirTurn = true;
+                                        gameUI.startTimer();
+                                    }
+                                    gameUI.updateEnemyBoard(outcome, coordinates);
+                                } else {
+                                    //update own board
+                                    if (!outcome.equals("hit") && !outcome.startsWith("destroyed")) {
+                                        isTheirTurn = true;
+                                        gameUI.appendMessage("It's your turn to play.", "GAME");
+                                        gameUI.startTimer();
+                                    }
+                                    gameUI.updateOwnBoard(outcome, coordinates);
+                                }
+                                break;
+                            case "MoveEnded":
+                                isTheirTurn = true;
+                                gameUI.appendMessage("Enemy ran out of time.", "GAME");
+                                gameUI.startTimer();
+                                break;
+                            case "GameStart":
+                                shipPlacement.startGame();
+                                break;
+                            case "PlayerBusy":
+                                isBusy = false;
+                                lobbyFrame.enablePlayButton(true);
+                                JOptionPane.showMessageDialog(null, "Player is busy. Please try again later",
+                                        "Error", JOptionPane.ERROR_MESSAGE);
+                                break;
+                            default:
+                                System.out.println(input);
+                                break;
+                        }
+                    }
+                }
+            } catch (EOFException e) {
+                // EOFException - if this input stream reaches the end
+                // before reading eight bytes
+            } catch (ClassCastException e) {
+                e.printStackTrace();
+                System.out.println("An error occurred.");
+                System.exit(1);
+            } catch (ClassNotFoundException | IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
